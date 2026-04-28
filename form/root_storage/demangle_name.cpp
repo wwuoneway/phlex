9a1a964384bafbc9e1f0b183cfe2dd248a9d9953
Chris Green <greenc@fnal.gov>
2026-04-28 14:46:05 -0500

    `clang-tidy`: resolve `cppcoreguidelines-no-malloc` (#548)
    * `clang-tidy`: resolve `cppcoreguidelines-no-malloc`
    
    * Report on `clang-tidy` fix progress
    
    * Use `std::unique_ptr`/RAII to handle malloc'd memory.
    
    - Per https://github.com/Framework-R-D/phlex/pull/548#discussion_r3132302164

--- form/root_storage/demangle_name.cpp
+++ form/root_storage/demangle_name.cpp
@@ -1,18 +1,32 @@
 #include "TClassEdit.h"
 
+#include "demangle_name.hpp"
+
+#include <memory>
+
+namespace {
+  class char_string_holder {
+  public:
+    char_string_holder(char* cstr) : m_cstr(cstr, std::free) {}
+    operator char const*() const { return m_cstr.get(); }
+
+  private:
+    std::unique_ptr<char, decltype(&std::free)> m_cstr;
+  };
+}
+
 namespace form::detail::experimental {
   // Return the demangled type name
   std::string DemangleName(std::type_info const& type)
   {
     int errorCode{};
     // The TClassEdit version works on both linux and Windows.
-    char* demangledName = TClassEdit::DemangleTypeIdName(type, errorCode);
+    auto const demangledName = char_string_holder{TClassEdit::DemangleTypeIdName(type, errorCode)};
     if (errorCode != 0) {
       // NOTE: Instead of throwing, we could return the mangled name as a fallback.
       throw std::runtime_error("Failed to demangle type name");
     }
     std::string result(demangledName);
-    std::free(demangledName);
     return result;
   }
 }
